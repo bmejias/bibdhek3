7505588291a0b73b304e70a092fdf3166625695b
Boriss Mejias <boriss.mejias@enterprisedb.com>
2024-11-17 11:49:39 +0100

    Add skeleton for the application

---
+++ database/app/app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('BIBDHEK_KEY')
+
+@app.route('/books')
+def books():
+    return render_template('books.html')
+
+@app.route('/loans')
+def loans():
+    return render_template('loans.html')
+
+@app.route('/users')
+def users():
+    return render_template('users.html')
+
+@app.route('/add_book')
+def add_book():
+    return render_template('add_book.html')
+
+@app.route('/add_user')
+def add_user():
+    return render_template('add_user.html')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
